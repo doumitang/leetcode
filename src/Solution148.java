60a469916b46d7eeedb31deff2016916f152c8d7
jq y <137626233@qq.com>
2020-01-08 16:46:45 +0800

    solution147 and 148

--- src/Solution148.java
+++ src/Solution148.java
@@ -1,11 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 class Solution148 {
     public static class ListNode {
         int val;
@@ -13,6 +5,13 @@ class Solution148 {
         ListNode(int x) { val = x; }
     }
 
+    /**
+     * 归并排序的分治思想，合并左排序结果和右排序结果，时间O(nlogn)，空间因为是链表所以O(1)
+     * Runtime: 2 ms, faster than 99.81% of Java online submissions for Sort List.
+     * Memory Usage: 41 MB, less than 57.89% of Java online submissions for Sort List.
+     * @param head
+     * @return
+     */
     public ListNode sortList(ListNode head) {
         int len = 0;
         ListNode p = head;
@@ -57,7 +56,6 @@ class Solution148 {
         }
         if (left == null) tail.next = right;
         if (right == null) tail.next = left;
-        ListNode p = headNew;
         /*while (p != null) {
             System.out.print("" + p.val + "->");
             p = p.next;
@@ -68,17 +66,16 @@ class Solution148 {
 
     public static void main(String[] args) {
         ListNode head = new ListNode(-1);
-        head.next = new ListNode(5);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(0);
-        /*ListNode p = head;
+        head.next = new ListNode(3);
+        head.next.next = new ListNode(4);
+        head.next.next.next = new ListNode(1);
+
+
+        Solution148 solution148 = new Solution148();
+        ListNode p = solution148.sortList(head.next);
         while (p != null) {
             System.out.print("" + p.val + "->");
             p = p.next;
-        }*/
-        System.out.println();
-        Solution148 solution148 = new Solution148();
-        solution148.sortList(head);
+        }
     }
 }
